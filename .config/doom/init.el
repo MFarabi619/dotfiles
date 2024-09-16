7056e610064561c7678536ff211cc67c1c047da9
Mumtahin Farabi <mfarabi619@gmail.com>
2024-09-16 12:19:29 -0400

    feat(emacs/init): activate ibuffer plugin

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -29,7 +29,7 @@
        vertico           ; the search engine of the future
 
        :ui
-       ;; deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
@@ -69,7 +69,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
