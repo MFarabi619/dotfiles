ba930c16114be64cf189b79ff99d2797130b5497
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-13 16:59:52 -0500

    feat(doom/custom): add docker lsp

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lsp-docker empv))
  '(warning-suppress-types '((lsp-mode) (defvaralias) (lexical-binding))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
