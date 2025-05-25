a31ad330857295bf422446778276b451e64d4831
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-25 14:51:24 -0400

    feat(doom/init/solidity): disable module

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -214,7 +214,7 @@
            +lsp
            +tree-sitter)
        ;;sml
-       (solidity +lsp)    ; do you need a blockchain? No.
+       ;; (solidity +lsp)    ; do you need a blockchain? No.
        ;;swift            ; who asked for emoji variables?
        ;;terra            ; Earth and Moon in alignment for performance.
        (web +lsp
